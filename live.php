5a7b9696f9e33b0c70595366d247fdfd8e113dff
Jano <164564780+David-jano@users.noreply.github.com>
2024-12-17 02:11:08 -0800

    i have just added new ajax and jquery codes

---
+++ live.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="search-div">
+<input type="text" name="write" id="search-field">
+    </div>
+
+    <script type="text/javascript">
+$(document).ready(function(){
+    $("#search-field").keyup(function(){
+
+    var input = $(this).val();
+   alert(input);
+    
+});
+});
+        </script>
+</body>
+</html>
